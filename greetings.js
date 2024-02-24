71d252be32cd8cbf1d27ae2a0a07b96d7d621827
Saira-Asif <160786613+Saira-Asif@users.noreply.github.com>
2024-02-24 18:45:10 +0500

    Add files via upload

---
+++ greetings.js
@@ -0,0 +1,3 @@
+// Hello, World! program in TypeScript
+var greetings = "Hello, World!";
+console.log(greetings);
---
+++ greetings.ts
@@ -0,0 +1,4 @@
+// Hello, World! program in TypeScript
+const greetings: string = "Hello, World!";
+console.log(greetings)
+
